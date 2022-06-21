240cad6e3debb90843276f6d0f7a61ad38b81bba
elias-dzobo <elias.dzobo@gmail.com>
2022-06-21 20:22:00 +0000

    Predict page Complete

---
+++ app.py
@@ -0,0 +1,7 @@
+from matplotlib.pyplot import show
+import streamlit as st 
+from predict_page import show_predict_page 
+
+st.sidebar.selectbox("Explore or Predict", ('Predict', 'Explore'))
+
+show_predict_page() 
